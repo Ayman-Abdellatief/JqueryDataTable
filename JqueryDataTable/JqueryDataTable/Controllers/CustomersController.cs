542c7f12434c9888414ad8a57165e5900b921f88
ayman.abdellatief <ayman.abdellatief@it-blocks.com>
2023-02-09 11:27:36 +0200

    Build table and init Data

---
+++ JqueryDataTable/JqueryDataTable/Controllers/CustomersController.cs
@@ -0,0 +1,33 @@
+﻿using JqueryDataTable.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JqueryDataTable.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CustomersController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public IActionResult GetCustomers()
+        {
+            var customers = _context.Customers.ToList();
+
+            var recordsTotal = customers.Count();
+
+            var jsonData = new { recordsFilterd = recordsTotal, recordsTotal, data = customers };
+            return Ok(jsonData);
+        }
+    }
+}
